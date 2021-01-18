3167638d13f78b24bf5cf7754dfaf35a2207ab35
shweta388 <shwetasahay449@gmail.com>
2021-01-18 10:38:33 -0800

    search

--- components/basicComponents/InputText.jsx
+++ components/basicComponents/InputText.jsx
@@ -1,10 +1,14 @@
-function InputText({ type, name, placeholder, className }) {
+function InputText({ type, name, placeholder, className, getValue }) {
+  const handleChange = (e) => {
+    getValue(e.target.value);
+  };
   return (
     <input
       type={type}
       name={name}
       placeholder={placeholder}
       className={className}
+      onChange={handleChange}
     />
   );
 }
--- components/searchbar/SearchBar.jsx
+++ components/searchbar/SearchBar.jsx
@@ -1,8 +1,19 @@
 import InputText from "@components/basicComponents/InputText";
 import Button from "../basicComponents/Button";
 import Link from "next/link";
+import { useState } from "react";
 
 function SearchBar() {
+  const [search, setSearch] = useState("");
+  const getValue = (val) => {
+    setSearch(val);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(search);
+  };
+
   return (
     <div className="searchbar">
       <h3>¿Listo para encontrar su próximo auto?</h3>
@@ -10,11 +21,13 @@ function SearchBar() {
         <div className="fieldrow">
           <InputText
             type="text"
-            name=""
+            name="search"
+            getValue={getValue}
             placeholder="Busque por modelo, marca o una palabra clave"
             className="form-control custfield"
+            value={search}
           />
-          <Button className="sbtn" name="Buscar" />
+          <Button className="sbtn" name="Buscar" onClick={handleClick} />
           <Link href="/">
             <a className="extfilter float-right">Búsqueda avanzada</a>
           </Link>
